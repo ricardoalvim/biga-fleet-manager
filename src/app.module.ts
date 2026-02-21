cfaa9011590a24fa10f6bac5d7efa9ef6447e4fe
Ricardo Alvim <ricardo.alvim@comparegames.com.br>
2026-02-21 16:11:59 -0300

    feat: added basic new modules items

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common'
+import { RedisModule } from './shared/infrastructure/redis/redis.module';
+import { GatewayModule } from './modules/gateway/gateway.module';
+import { TelemetryModule } from './modules/telemetry/telemetry.module';
+import { FleetModule } from './modules/fleet/fleet.module';
+import { MaintenanceModule } from './modules/maintenance/maintenance.module';
 
 @Module({
-  imports: [],
+  imports: [RedisModule, GatewayModule, TelemetryModule, FleetModule, MaintenanceModule],
   controllers: [],
   providers: [],
 })
